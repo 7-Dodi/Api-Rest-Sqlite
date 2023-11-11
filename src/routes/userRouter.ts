421f31c3c6ca5f04cdee5e525125c2726b4e5cf7
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-11-10 21:27:32 -0300

    refactor: Establishing authentication and middeware routes

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,9 +1,11 @@
 const express = require('express');
 const userRouter = express.Router();
-import {listUser, addUser} from '../controllers/userController';
+import checkToken from '../middleware/checkToken';
+import {listUser, addUser, authenticationUser} from '../controllers/userController';
 
 //Métodos do usuário:
-userRouter.get('/', listUser);
+userRouter.get('/', checkToken, listUser);
 userRouter.post('/', addUser);
+userRouter.post('/authentication', authenticationUser);
 
 export {userRouter};
